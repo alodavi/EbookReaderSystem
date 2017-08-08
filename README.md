dcb8019b221329ecd68fe9e8d736fb9373fe2e75
Aloisia Davi <aloisia.davi@bonial.com>
2017-08-08 13:33:22 +0200

    -implemented some functions in UserManagerService - updated Readme.md

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # EbookReaderSystem
+This is the functional version of the solution to this problem in the book _Cracking the code Interview_.
+
+##Functionalities:
+- User membership creation and extention
+- Search the database of books
+- Reading a book
+- Only one active user at a time
+- Only one active book by this use
--- src/main/scala/ebook/service/UserManagerService.scala
+++ src/main/scala/ebook/service/UserManagerService.scala
@@ -1,16 +1,19 @@
 package ebook.service
 
-import ebook.domain.models.{User, UserManager}
+import ebook.domain.models.{ User, UserManager}
 
 /**
   * Created by aloisia.davi on 07.08.17.
   */
 trait UserManagerService extends UserManager{
 
-  def addUser(id:Int, details:String, accountType:Int):User
+  def addUser(id:Int, details:String)(implicit userManager: UserManager):Option[User] = {
+    if(!userManager.users.contains(id)) Some(User(id, details)) else None
+  }
 
-  def find(id:Int):User
+  def find(id:Int)(implicit userManager: UserManager):Option[User] = userManager.users.get(id)
 
+//
   def remove(user: User):Boolean
 
   def remove(id:Int):Boolean
